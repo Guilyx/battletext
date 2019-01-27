c136d642ac767606dc95360315a87f40a3643508
Erwin Lejeune <43545812+Guilyx@users.noreply.github.com>
2019-01-28 00:51:16 +0100

    Update
    Added inventory class, item use, hp/mana round iterated bars

--- battle_game/Classes/CharacterClass.py
+++ battle_game/Classes/CharacterClass.py
@@ -13,7 +13,8 @@ class ColorsBook:
 
 
 class Character:
-    def __init__(self, hp, mp, atk, dfs, magic):
+    def __init__(self, name, hp, mp, atk, dfs, item):
+        self.name = name
         self.maxhp = hp
         self.hp = hp
         self.maxmp = mp
@@ -21,16 +22,38 @@ class Character:
         self.atk_low = atk - 10
         self.atk_high = atk + 10
         self.dfs = dfs
-        self.magic = magic
-        self.actions = ["Attack", "Magic"]
+        self.item = item
+        self.aesthetics = "    "
+        self.magic = [{"name": "Fire", "cost": 120, "damage": 370, "type": "Offense"},
+                 {"name": "Thunder", "cost": 100, "damage": 255, "type": "Offense"},
+                 {"name": "Hurricane", "cost": 35, "damage": 150, "type": "Offense"},
+                 {"name": "Quake", "cost": 180, "damage": 405, "type": "Offense"},
+                 {"name": "Blizzard", "cost": 80, "damage": 125, "type": "Offense"},
+                 {"name": "First Heal", "cost": 150, "damage": 260, "type": "Support"},
+                 {"name": "Partial Heal", "cost": 300, "damage": 400, "type": "Support"}]
+        self.actions = ["Attack", "Magic", "Items"]
+
+
+    def getmagic(self):
+        return self.magic
 
     def generate_damage(self):
         return random.randrange(self.atk_low, self.atk_high)
 
     def get_spelldamage(self, index):
-        magic_low = self.magic[index]["damage"] - 5
-        magic_high = self.magic[index]["damage"] + 5
+        magic_low = self.magic[index]["damage"] - 10
+        magic_high = self.magic[index]["damage"] + 10
         return random.randrange(magic_low, magic_high)
+
+    def get_spelltype(self, index):
+        spelltype = self.magic[index]["type"]
+        return spelltype
+
+    def heal(self, damage):
+        self.hp += damage
+        if self.hp > self.maxhp:
+            self.hp = self.maxhp
+        return self.hp
 
     def damage_taken(self, damage):
         self.hp -= damage
@@ -69,7 +92,7 @@ class Character:
         index = 0
         print("Actions")
         for item in self.actions:
-            print(str(index) + " : ", item)
+            print(self.aesthetics, str(index) + " : ", item)
             index += 1
 
         print("========================")
@@ -81,25 +104,81 @@ class Character:
     def choose_spell(self):
         print("========================")
         index = 0
-        print("Magic Spells")
+        print(ColorsBook.OKBLUE + ColorsBook.BOLD +
+              "SPELLS" + ColorsBook.ENDC)
         for spell in self.magic:
-            print(str(index) + " : ", spell["name"],
-                  " (cost :", str(spell["cost"]) + " ) ")
+            print(self.aesthetics, str(index) + " : ", spell["name"],
+                  " ( cost :", str(spell["cost"]), ")",
+                  "type : ", str(spell["type"]) + " ) ")
             index += 1
+        print("========================")
         choice = input("Choose your spell | ")
-        print("You chose : " + choice)
         return(int(choice))
 
-    def printstats(self, name):
+    def choose_item(self):
+        print("========================")
         index = 0
-        print(name)
+        print(ColorsBook.WARNING + ColorsBook.BOLD +
+              "ITEMS" + ColorsBook.ENDC)
+        for item in self.item:
+            print(self.aesthetics, str(index) + " : ", item["item"].name, " : ",
+                  item["item"].description, " (*", item["quantity"], ")")
+            index += 1
         print("========================")
-        print("HP :", self.maxhp)
-        print("MP :", self.maxmp)
-        print("ATK MAX :", self.atk_high)
-        print("DFS :", self.dfs)
-        print("MAGIC SPELLS :")
-        for spell in self.magic:
-            print("Name : ", spell["name"], "|| Damage : ",
-                  spell["damage"], " points")
-        print("========================")
+        choice = input("Choose your item | ")
+        return(int(choice))
+
+
+    def printstats(self):
+        hp_bar = ''
+        mana_bar = ''
+        bar_ticks = (self.hp / self.maxhp) * 25
+        mana_bar_ticks = (self.mp / self.maxmp) * 10
+
+        while bar_ticks > 0:
+            hp_bar += "█"
+            bar_ticks -= 1
+
+        while len(hp_bar) < 25:
+            hp_bar += " "
+
+        while mana_bar_ticks > 0:
+            mana_bar += "█"
+            mana_bar_ticks -= 1
+
+        while len(mana_bar) < 10:
+            mana_bar += " "
+
+        hp_string = str(self.hp) + "/" + str(self.maxhp)
+        mp_string = str(self.mp) + "/" + str(self.maxmp)
+
+        current_mp = ""
+        current_hp = ""
+
+        if len(hp_string) < 9:
+            decreased = 9 - len(hp_string)
+            while decreased > 0:
+                current_hp += " "
+                decreased -= 1
+            current_hp += hp_string
+
+        else:
+            current_hp = hp_string
+
+        if len(mp_string) < 7:
+            decreased = 7 - len(mp_string)
+            while decreased > 0:
+                current_mp += " "
+                decreased -= 1
+            current_mp += mp_string
+
+        else:
+            current_mp = mp_string
+
+        print("NAME                 HP                                    MP")
+        print("                      _________________________             __________")
+        print(ColorsBook.BOLD + self.name + "   " +
+              current_hp + " |" + ColorsBook.OKGREEN + hp_bar +
+              ColorsBook.ENDC + ColorsBook.BOLD + "|   " +
+              mp_string + " |" + ColorsBook.OKBLUE + mana_bar + ColorsBook.ENDC + "|")
+        print("\n\n")
--- battle_game/main.py
+++ battle_game/main.py
@@ -25,31 +25,42 @@
 
 
 from Classes.CharacterClass import*
+from Classes.Inventory import*
 from random import*
 import time
 
 #==============================================================================#
 
-magic = [{"name": "Katon", "cost": 30, "damage": 150},
-         {"name": "Raiton", "cost": 20, "damage": 105},
-         {"name": "Fuuton", "cost": 5, "damage": 50},
-         {"name": "Mokuton", "cost": 80, "damage": 205},
-         {"name": "Suiton", "cost": 10, "damage": 65}]
+#Items
+potion = Item("Potion", "potion", "Heals 50 HP", 50)
+superpotion = Item("Super-Potion", "potion", "Heals 100 HP", 100)
+megapotion = Item("Mega-Potion", "potion", "Heals 500 HP", 500)
+elixer = Item("Elixer", "elixer", "Fully restores HP/MP of one party member", 99999)
+ether = Item("Ether", "elixer", "Restores party's HP/MP", 999999)
+sacredashes = Item("SacredAshes", "offense", "Deals 500 damage", 500)
+
+playeritems = [{"item": potion, "quantity": 15},
+               {"item": elixer, "quantity": 1},
+               {"item": sacredashes, "quantity": 3}]
+enemyitems = [{"item": potion, "quantity": 15},
+              {"item": megapotion, "quantity": 8},
+              {"item": sacredashes, "quantity": 5}]
 
 #Character(HP, MP, ATTACK, DEFENSE, MAGICSPELL)
+player1 = Character("AURAZUR:", 4700, 800, 330, 50, playeritems)
+player2 = Character("BOB:    ", 400, 1000, 300, 45, playeritems)
+enemy = Character("TRUMP:  ", 7500, 280, 515, 20, enemyitems)
 
-player = Character(800, 200, 100, 50, magic)
-enemy = Character(1400, 80, 70, 20, magic)
 
 enemy_currenthp = enemy.get_hp()
 enemy_currentmp = enemy.get_mp()
-player_currenthp = player.get_hp()
-player_currentmp = player.get_mp()
+player_currenthp = player1.get_hp()
+player_currentmp = player1.get_mp()
 
 #==============================================================================#
-
 def printspaces():
     print("========================")
+
 
 def turndelimitation():
     print(ColorsBook.OKGREEN + ColorsBook.BOLD +
@@ -69,13 +80,15 @@ def randomevent(percentage):
     if rdn <= (percentage):
         return True
 
-def player_turn(enemy_currenthp, player_currentmp):
-    action_choice = player.choose_action()
+def player_turn(player_currenthp, enemy_currenthp, player_currentmp,
+                enemy_currentmp):
+
+    action_choice = player1.choose_action()
     printspaces()
     #IF ATTACK (CLOSE COMBAT)
     if action_choice == 0 :
         crit = randomevent(5)
-        damage = player.generate_damage()
+        damage = player1.generate_damage()
         if crit == True:
             print(ColorsBook.FAIL + ColorsBook.BOLD + "CRITICAL STRIKE" +
                   ColorsBook.ENDC)
@@ -86,28 +99,42 @@ def player_turn(enemy_currenthp, player_currentmp):
         enemy_currenthp = enemy.damage_taken(damage)
         print("The enemy has " + ColorsBook.OKGREEN + str(enemy_currenthp) +
               ColorsBook.ENDC + " HP left.")
-        return(enemy_currenthp, player_currentmp)
+        return(player_currenthp, enemy_currenthp, player_currentmp,
+               enemy_currentmp)
         pass
 
     #IF MAGIC
-    if action_choice == 1:
-        spell_choice = player.choose_spell()
+    elif action_choice == 1:
+        spell_choice = player1.choose_spell()
         printspaces()
-        print("You chose : | ", player.get_spellname(spell_choice), " |")
-        cost = player.get_spellcost(spell_choice)
+        print("You chose : | ", player1.get_spellname(spell_choice), " |")
+        cost = player1.get_spellcost(spell_choice)
 
         if (cost <= player_currentmp):
-            player_currentmp = player.consumed_mp(cost)
-            magic_damage = player.get_spelldamage(spell_choice)
-            print("Your spell has hit the enemy by " + ColorsBook.FAIL +
-                  str(magic_damage) + ColorsBook.ENDC + " damage points !!")
-            enemy_currenthp = enemy.damage_taken(magic_damage)
-            print("You have " + ColorsBook.OKBLUE + str(player_currentmp) +
-                  ColorsBook.ENDC + " MP left.")
-            print("The enemy has " + ColorsBook.OKGREEN + str(enemy_currenthp) +
-                  ColorsBook.ENDC + " HP left.")
-            return(enemy_currenthp, player_currentmp)
+            player_currentmp = player1.consumed_mp(cost)
+            magic_damage = player1.get_spelldamage(spell_choice)
+
+            if (player1.get_spelltype(spell_choice) == "Offense"):
+                print("Your spell has hit the enemy by " + ColorsBook.FAIL +
+                      str(magic_damage) + ColorsBook.ENDC + " damage points !!")
+                enemy_currenthp = enemy.damage_taken(magic_damage)
+                print("You have " + ColorsBook.OKBLUE + str(player_currentmp) +
+                      ColorsBook.ENDC + " MP left.")
+                print("The enemy has " + ColorsBook.OKGREEN + str(enemy_currenthp) +
+                      ColorsBook.ENDC + " HP left.")
+                return(player_currenthp, enemy_currenthp, player_currentmp,
+                       enemy_currentmp)
+
+            if (player1.get_spelltype(spell_choice) == "Support"):
+                print("You have recovered " + ColorsBook.OKGREEN +
+                      str(magic_damage) + ColorsBook.ENDC + " HP !")
+                player_currenthp = player1.heal(magic_damage)
+                print("You have " + ColorsBook.OKGREEN + str(player_currenthp) +
+                      ColorsBook.ENDC + " HP left.")
+                return(player_currenthp, enemy_currenthp, player_currentmp,
+                       enemy_currentmp)
             pass
+
         else:
             print(ColorsBook.FAIL + ColorsBook.BOLD +
                  "You don't have enough MP !!!!" + "( " + ColorsBook.OKBLUE +
@@ -115,12 +142,53 @@ def player_turn(enemy_currenthp, player_currentmp):
             printdots()
             print("You wasted a round. The enemy has " + str(enemy_currenthp) +
                   " HP left.")
-            return(enemy_currenthp, player_currentmp)
+            return(player_currenthp, enemy_currenthp, player_currentmp,
+                   enemy_currentmp)
             pass
 
         pass
 
-def enemy_turn(player_currenthp, enemy_currentmp):
+    #IF ITEMS
+    elif action_choice == 2:
+        item_choice = player1.choose_item()
+        item = player1.item[item_choice]["item"]
+
+        if player1.item[item_choice]["quantity"] == 0:
+            print(ColorsBook.FAIL + "NONE LEFT !!!" + ColorsBook.ENDC)
+            return(player_currenthp, enemy_currenthp, player_currentmp,
+                   enemy_currentmp)
+            pass
+
+        else:
+            player1.item[item_choice]["quantity"] -= 1
+
+            if item.type == "potion":
+                player_currenthp = player1.heal(item.prop)
+                print(ColorsBook.WARNING + item.name + " heals you for ",
+                      str(item.prop) + " HP (yay)." + ColorsBook.ENDC)
+                pass
+
+            elif item.type == "elixer":
+                player_currenthp = player1.heal(item.prop)
+                player_currentmp = player1.maxmp
+                print(ColorsBook.WARNING + item.name +
+                      " has fully restored your HP and MP" + ColorsBook.ENDC)
+                pass
+
+            elif item.type == "offense":
+                enemy_currenthp = enemy.damage_taken(item.prop)
+                print(ColorsBook.WARNING + item.name +
+                      " has dealt " + str(item.prop) + " damage to the enemy." +
+                      ColorsBook.ENDC)
+                pass
+
+        return(player_currenthp, enemy_currenthp, player_currentmp,
+               enemy_currentmp)
+        pass
+
+
+def enemy_turn(player_currenthp, enemy_currenthp, player_currentmp,
+               enemy_currentmp):
     action_choice = randint(0,1)
     nomana = 0
 
@@ -135,28 +203,43 @@ def enemy_turn(player_currenthp, enemy_currentmp):
         print("The enemy has hit you for " + ColorsBook.FAIL + str(damage) +
               ColorsBook.ENDC + " damage points !")
         printdots()
-        player_currenthp = player.damage_taken(damage)
+        player_currenthp = player1.damage_taken(damage)
         print("You have " + ColorsBook.OKGREEN + ColorsBook.BOLD +
               str(player_currenthp) +  ColorsBook.ENDC + " HP left.")
-        return(player_currenthp, enemy_currentmp)
+        return(player_currenthp, enemy_currenthp, player_currentmp,
+               enemy_currentmp)
         pass
 
     if action_choice == 1:
         spell_choice = randint(0, 4)
-        print("The enemy casts | ", player.get_spellname(spell_choice), " |")
+        print("The enemy casts | ", player1.get_spellname(spell_choice), " |")
         time.sleep(2)
         cost = enemy.get_spellcost(spell_choice)
 
         if (cost <= enemy_currentmp):
             enemy_currentmp = enemy.consumed_mp(cost)
             magic_damage = enemy.get_spelldamage(spell_choice)
-            print("His spell hits you hard...  " + ColorsBook.FAIL +
-                  str(magic_damage) + ColorsBook.ENDC + " damage points !!")
-            player_currenthp = player.damage_taken(magic_damage)
-            print("You have " + ColorsBook.OKGREEN + ColorsBook.BOLD +
-                  str(player_currenthp) + ColorsBook.ENDC + " HP left.")
-            return(player_currenthp, enemy_currentmp)
+
+            if (enemy.get_spelltype(spell_choice) == "Offense"):
+                print("His spell hits you hard...  " + ColorsBook.FAIL +
+                      str(magic_damage) + ColorsBook.ENDC + " damage points !!")
+                player_currenthp = player1.damage_taken(magic_damage)
+                print("You have " + ColorsBook.OKGREEN + ColorsBook.BOLD +
+                      str(player_currenthp) + ColorsBook.ENDC + " HP left.")
+                return(player_currenthp, enemy_currenthp, player_currentmp,
+                       enemy_currentmp)
+
+            if (enemy.get_spelltype(spell_choice) == "Support"):
+                print("The enemy has recovered " + ColorsBook.OKGREEN +
+                      str(magic_damage) + ColorsBook.ENDC + " HP !")
+                enemy_currenthp = enemy.heal(magic_damage)
+                print("He now has " + ColorsBook.OKGREEN + str(enemy_currenthp) +
+                      ColorsBook.ENDC + " HP left.")
+                return(player_currenthp, enemy_currenthp, player_currentmp,
+                       enemy_currentmp)
+
             pass
+
         elif (cost > enemy_currentmp) and (nomana == 0):
             print(ColorsBook.FAIL + ColorsBook.BOLD + "The enemy tried to " +
                   "cast a spell... but he doesn't have enough MP !!" +
@@ -166,25 +249,22 @@ def enemy_turn(player_currenthp, enemy_currentmp):
                   str(player_currenthp) + ColorsBook.ENDC + " HP left.")
             if enemy_currentmp == 0:
                 nomana = 1
-            return(player_currenthp, enemy_currentmp)
+            return(player_currenthp, enemy_currenthp, player_currentmp,
+                   enemy_currentmp)
             pass
 
         pass
 
 
 #==============================================================================#
-
-player.printstats("AURAZUR")
-turndelimitation()
-enemy.printstats("ENEMY")
-
-input("Press Enter to continue...")
-
-time.sleep(5)
-
 printspaces()
 print(ColorsBook.FAIL + ColorsBook.BOLD + "AN ENEMY ATTACKS!" + ColorsBook.ENDC)
 
+input("Press Enter to continue...")
+player1.printstats()
+printspaces()
+enemy.printstats()
+time.sleep(3)
 #==============================================================================#
 
 who = whoplays()
@@ -192,9 +272,14 @@ who = whoplays()
 while True:
     if who == 1:
         print(ColorsBook.BOLD + "It's your turn !" + ColorsBook.ENDC)
-        enemy_currenthp, player_currentmp = player_turn(enemy_currenthp,
-                                                        player_currentmp)
+        player_currenthp, enemy_currenthp, player_currentmp, enemy_currentmp = player_turn(player_currenthp,
+                                                                                           enemy_currenthp,
+                                                                                           player_currentmp,
+                                                                                           enemy_currentmp)
         turndelimitation()
+        player1.printstats()
+        printspaces()
+        enemy.printstats()
         if enemy_currenthp == 0:
             break
         who = 2
@@ -202,14 +287,20 @@ while True:
     if who == 2:
         print("The enemy is about to make a move ... *roulement de tambourins*")
         printdots()
-        time.sleep(5)
+        time.sleep(3)
         print(ColorsBook.BOLD + "It's the enemy's turn !" + ColorsBook.ENDC)
-        player_currenthp, enemy_currentmp = enemy_turn(player_currenthp,
-                                                       enemy_currentmp)
+        player_currenthp, enemy_currenthp, player_currentmp, enemy_currentmp = enemy_turn(player_currenthp,
+                                                                                           enemy_currenthp,
+                                                                                           player_currentmp,
+                                                                                           enemy_currentmp)
         turndelimitation()
+        player1.printstats()
+        printspaces()
+        enemy.printstats()
         if player_currenthp == 0:
             break
-        time.sleep(5)
+
+        time.sleep(3)
         who = 1
 
 #==============================================================================#
